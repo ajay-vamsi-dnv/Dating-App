b6224d22b30c05bc7bea08efc92ee62244adc3d2
Ajay Vamsi DNV <ajay.vamsi1996@gmail.com>
2021-11-24 00:27:13 +0530

    End of Section 2
    End of Section Two
    
    - Created API File Structure
    - Created Database for DatingApp
    - Wrote Sample End Points on Users Table

---
+++ API/SQLQueries/Insert rows into table 'Users'.sql
@@ -0,0 +1,14 @@
+-- Insert rows into table 'TableName'
+INSERT INTO Users
+    ( -- columns to insert data into
+    [UserName]
+    )
+VALUES
+    ( -- first row: values for the columns in the list above
+        'Ajay96'
+),
+    ( -- second row: values for the columns in the list above
+        'Vamsi96'
+)
+-- add more rows here
+GO
